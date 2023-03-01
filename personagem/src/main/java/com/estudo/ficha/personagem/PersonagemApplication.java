f50e79a529502a7cf3f6eadac27d197d831abb46
Rai Rafael <rairafaelss@gmail.com>
2023-03-01 17:16:06 -0300

    Adicionado Servico Personagem

---
+++ personagem/src/main/java/com/estudo/ficha/personagem/PersonagemApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha.personagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonagemApplication.class, args);
+	}
+
+}
---
+++ personagem/src/test/java/com/estudo/ficha/personagem/PersonagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha.personagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
