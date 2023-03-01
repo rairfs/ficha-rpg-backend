5be10318c41ce96671f956742d5868ee3adc66ab
Rai Rafael <rairafaelss@gmail.com>
2023-03-01 16:24:32 -0300

    Primeiro Commit

---
+++ src/main/java/com/estudo/ficha/FichaRpgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaRpgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichaRpgBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/ficha/FichaRpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaRpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
