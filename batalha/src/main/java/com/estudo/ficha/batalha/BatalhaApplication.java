a30afa6646c1f804268d2b1a215211db0c584c5d
Rai Rafael <rairafaelss@gmail.com>
2023-03-01 18:13:14 -0300

    Adicionado servicos restantes

---
+++ batalha/src/main/java/com/estudo/ficha/batalha/BatalhaApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha.batalha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatalhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatalhaApplication.class, args);
+	}
+
+}
