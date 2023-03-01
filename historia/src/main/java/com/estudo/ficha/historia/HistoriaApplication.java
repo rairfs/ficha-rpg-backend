88b81ab6ac873fb455636992f70c6bb2775422c1
Rai Rafael <rairafaelss@gmail.com>
2023-03-01 18:15:21 -0300

    Adicionada config Servicos

---
+++ historia/src/main/java/com/estudo/ficha/historia/HistoriaApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha.historia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaApplication.class, args);
+	}
+
+}
---
+++ mapa/src/main/java/com/estudo/ficha/mapa/MapaApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ficha.mapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaApplication.class, args);
+	}
+
+}
